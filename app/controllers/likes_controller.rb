3b7c0e587ff92bf6a9487ead919ad96185efe00e
maxbarsukov <maximbarsukov@bk.ru>
2021-06-18 15:47:34 +0300

    Like controller updated

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
   def save_like
     @like = Like.new(post_id: params[:post_id], user_id: current_user.id)
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit]
 
-  def index
-    @posts = Post.order('created_at DESC')
-  end
-
   def show
   end
 
