e54f2f18a57ac369ff70651a96bbd66b6727c140
maxbarsukov <maximbarsukov@bk.ru>
2021-06-21 20:58:43 +0300

    Comments updated and words beraking fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-  def breaking_word_wrap(txt, col = 80)
-    txt.gsub(/(.{1,#{col}})( +|$\n?)|(.{1,#{col}})/,
-             "\\1\\3\n")
-  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,17 @@ class Comment < ApplicationRecord
   validates_presence_of :comment
   validates_presence_of :user_id
   validates_presence_of :post_id
+
+  after_create :increase_post_comments_counter
+  after_destroy :decrease_post_comments_counter
+
+  private
+
+  def increase_post_comments_counter
+    Post.find(self.post_id).increment(:total_comments_count).save
+  end
+
+  def decrease_post_comments_counter
+    Post.find(self.post_id).decrement(:total_comments_count).save
+  end
 end
