cea9dbfa9f934c6a303286686a197d59283a11be
maxbarsukov <maximbarsukov@bk.ru>
2021-06-21 17:34:00 +0300

    JS using with Turbolinks fixed

--- app/javascript/includes/posts.js
+++ app/javascript/includes/posts.js
@@ -1,13 +1,15 @@
-$(function() {
-    // jquery loaded
-    $(".post-like").on("click", function() {
-        var post_id = $(this).data("id");
+document.addEventListener("turbolinks:load", () => {
+    $(function () {
+        // jquery loaded
+        $(".post-like").on("click", function () {
+            var post_id = $(this).data("id");
 
-        $.ajax({
-            url: "/post/like/" + post_id,
-            method: "GET",
-        }).done(function(response) {
-            console.log(response);
-        })
+            $.ajax({
+                url: "/post/like/" + post_id,
+                method: "GET",
+            }).done(function (response) {
+                console.log(response);
+            })
+        });
     });
 });
