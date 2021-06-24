a1bff6a1c6a60c30ecb0ccdbb96a0a4a8b47d6b8
maxbarsukov <maximbarsukov@bk.ru>
2021-06-24 16:43:00 +0300

    Facebook omniauth updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook]
 
   mount_uploader :image, ImageUploader
 
