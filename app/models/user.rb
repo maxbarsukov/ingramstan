34f69a73b985a9c48e22ae86ea8ff8ee3ed44ec9
maxbarsukov <maximbarsukov@bk.ru>
2021-06-25 17:49:34 +0300

    Omniauth info providing updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,14 @@ class User < ApplicationRecord
   def total_following
     Follower.where(follower_id: id).count
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.name
+      user.image = auth.info.image
+      user.username = auth.info.name.gsub(' ', '')
+    end
+  end
 end
