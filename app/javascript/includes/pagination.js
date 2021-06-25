f618fed7b08ab35c2bc198fc4ecb6a729ccf02a1
maxbarsukov <maximbarsukov@bk.ru>
2021-06-25 19:51:29 +0300

    Ajax loader moved

--- app/javascript/includes/pagination.js
+++ app/javascript/includes/pagination.js
@@ -9,7 +9,7 @@ $(document).on('turbolinks:load', function () {
                 more_posts_url= $('.pagination .next a').attr('href');
             }
             if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
-                $('.pagination').html('<img src="/app/assets/gifs/ajax-loader.gif" alt="Loading..." title="Loading..." />');
+                $('.pagination').html('<img src="/images/ajax-loader.gif" alt="Loading..." title="Loading..." />');
                 $.getScript(more_posts_url);
             }
         });
