e39e3b41b25df5532bf4c58b9c9bd1b0bc752ab3
nyapsilon <maximbarsukov@bk.ru>
2021-07-11 12:55:25 +0300

    Fix CSRF problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protect_from_forgery
+
   protected
 
   def configure_permitted_parameters
