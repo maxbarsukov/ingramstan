3ebfdd7f0e38c76070c8985c2e837bc96b14cb1e
nyapsilon <maximbarsukov@bk.ru>
2021-07-11 12:58:48 +0300

    CSRF protection updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protect_from_forgery
+  protect_from_forgery with: :exception
 
   protected
 
